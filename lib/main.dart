6b316ed8e898f810e8e45454d33c465e2cc21c88
Parth Rathore <parthrathore018@gmail.com>
2025-09-20 16:37:12 +0530

    change data directory

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Isar database
-  final dir = await getApplicationCacheDirectory();
+  final dir = await getApplicationDocumentsDirectory();
   final isar = await Isar.open(
     [
       ConfigurationSchema,
